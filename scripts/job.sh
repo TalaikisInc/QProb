87dd5fb8e6d4eeedc07c134649339fe935c9c3d6
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2017-09-13 11:14:24 +0300

    universal job

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+COM=$1
+
 for PROJECT in bsnssnws entreprnrnws parameterless qprob realestenews stckmrkt webdnl
 do
         cd /home/$PROJECT
         source /usr/local/anaconda/bin/activate $PROJECT
-        /usr/local/anaconda/envs/$PROJECT/bin/python manage.py db_clean
+        /usr/local/anaconda/envs/$PROJECT/bin/python manage.py $COM
         source /usr/local/anaconda/bin/deactivate $PROJECT
 done
